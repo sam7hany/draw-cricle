0b2823b1e1c7652bbf604d145f81e2ed9eb7b052
sameh <39744797+sameh20130@users.noreply.github.com>
2020-12-19 20:04:56 +0200

    Add files via upload

---
+++ draw circle/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Cricle
+{
+    //by Excellent Programmer
+    //eppr.blogspot.com
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double radius;
+            double thickness = 0.4;
+            ConsoleColor BorderColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = BorderColor;
+            char symbol = '*';
+
+            do
+            {
+                Console.Write("Enter radius:::: ");
+                if (!double.TryParse(Console.ReadLine(), out radius) || radius <= 0)
+                {
+                    Console.WriteLine("radius have to be positive number");
+                }
+            }
+            while (radius <= 0);
+
+            Console.WriteLine();
+            double rIn = radius - thickness, rOut = radius + thickness;
+
+            for (double y = radius; y >= -radius; --y)
+            {
+                for (double x = -radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write(symbol);
+                    }
+                    else
+                    {
+                     
+                            Console.Write(" ");
+                        //by Excellent Programmer
+                        //eppr.blogspot.com
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
+//by Excellent Programmer
+//eppr.blogspot.com
